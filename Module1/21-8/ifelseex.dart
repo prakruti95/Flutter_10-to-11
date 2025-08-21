6b7909ee78d17aca764b30d66eda9a033479e1b9
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-08-21 11:00:55 +0530

    commit

---
+++ Module1/21-8/ifelseex.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  var marks = 26;
+
+    if(marks>35)
+    {
+      print("You are Pass");
+    }
+    else
+    {
+      print("You are Fail");
+    }
+}
---
+++ Module1/21-8/variableex.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  int a = 10;
+  print(a);
+
+  double b = 20;
+  print(b);
+
+  var c = "tops";
+  print(c);
+
+}
